a1762612eee4a21adff5ea06a2e8eb651756ea36
Vilius HTML24 <Vilius@HTML24-Vilius.(none)>
2014-09-23 12:29:51 +0200

    Moved league graph to stats page

--- src/TomGud/FoosLeader/CoreBundle/Controller/DefaultController.php
+++ src/TomGud/FoosLeader/CoreBundle/Controller/DefaultController.php
@@ -14,62 +14,14 @@ class DefaultController extends Controller
     	$latest_for_all = $result_repo->getLatestForAll();
     	$unconfirmed_for_user = $result_repo->getUnconfirmedResultsForUser($this->getUser());
 
-        $elo_repo = $this->getDoctrine()->getManager()->getRepository('FoosLeaderCoreBundle:ELOHistory');
-
-        // move this all somewhere
-        $elo_history_all_dates = $this->getEloDates($elo_repo->findEloHistoryForAll());
-        $elo_history_all_players = $this->getELODataPoints($elo_repo->findEloHistoryForAll());
 
         return $this->render('FoosLeaderCoreBundle:Default:index.html.twig',
         	array(
         		'latest_user' => $latest_for_user,
         		'latest_all' => $latest_for_all,
         		'unconfirmed' => $unconfirmed_for_user,
-        		'notifications' => array(),
-                'elo_history_all_dates' => $elo_history_all_dates,
-                'elo_history_all_players' => $elo_history_all_players,
+        		'notifications' => array()
         	)
         );
     }
-
-    public function getEloDates($eloHistories){
-        $dataPoints = array();
-
-        foreach ($eloHistories as $eloHistory) {
-            $date = $eloHistory->getRegistered();
-            $date = $date->format("Y-m-d");
-
-            if (!array_key_exists($date, $dataPoints)) {
-                $dataPoints[$date] = $date;
-            }
-        }
-        return $dataPoints;
-    }
-
-    public function getELODataPoints($eloHistories) {
-        $dataPoints = array();
-        $allDates = $this->getEloDates($eloHistories);
-        $allDates = array_flip($allDates);
-        foreach ($allDates as $key => $date) {
-            $allDates[$key] = -1;
-        }
-
-        foreach ($eloHistories as $eloHistory) {
-            $user = $eloHistory->getPlayer();
-            $date = $eloHistory->getRegistered();
-
-            $date = $date->format("Y-m-d");
-
-            if (!array_key_exists($user->getUsername(), $dataPoints)) {
-                $dataPoints[$user->getUsername()] = $allDates;
-            }
-
-            if ($dataPoints[$user->getUsername()][$date] < $eloHistory->getNewElo()) {
-                $dataPoints[$user->getUsername()][$date]  = $eloHistory->getNewElo();
-            }
-        }
-
-        return $dataPoints;
-    }
-
 }
--- src/TomGud/FoosLeader/CoreBundle/Controller/StatisticsController.php
+++ src/TomGud/FoosLeader/CoreBundle/Controller/StatisticsController.php
@@ -10,11 +10,63 @@ namespace TomGud\FoosLeader\CoreBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TomGud\FoosLeader\CoreBundle\Entity\ELOHistory;
 
 class StatisticsController  extends Controller {
 
     public function globalAction() {
-        return $this->render('FoosLeaderCoreBundle:Statistics:global.html.twig');
+
+        $elo_repo = $this->getDoctrine()->getManager()->getRepository('FoosLeaderCoreBundle:ELOHistory');
+
+        $elo_history_all_dates = $this->getEloDates($elo_repo->findEloHistoryForAll());
+        $elo_history_all_players = $this->getELODataPoints($elo_repo->findEloHistoryForAll());
+
+        return $this->render('FoosLeaderCoreBundle:Statistics:global.html.twig',
+            array(
+                'elo_history_all_dates' => $elo_history_all_dates,
+                'elo_history_all_players' => $elo_history_all_players,
+            )
+        );
     }
+    public function getEloDates($eloHistories){
+        $dataPoints = array();
+
+        foreach ($eloHistories as $eloHistory) {
+            $date = $eloHistory->getRegistered();
+            $date = $date->format("Y-m-d");
+
+            if (!array_key_exists($date, $dataPoints)) {
+                $dataPoints[$date] = $date;
+            }
+        }
+        return $dataPoints;
+    }
+
+    public function getELODataPoints($eloHistories) {
+        $dataPoints = array();
+        $allDates = $this->getEloDates($eloHistories);
+        $allDates = array_flip($allDates);
+        foreach ($allDates as $key => $date) {
+            $allDates[$key] = -1;
+        }
+
+        foreach ($eloHistories as $eloHistory) {
+            $user = $eloHistory->getPlayer();
+            $date = $eloHistory->getRegistered();
+
+            $date = $date->format("Y-m-d");
+
+            if (!array_key_exists($user->getUsername(), $dataPoints)) {
+                $dataPoints[$user->getUsername()] = $allDates;
+            }
+
+            if ($dataPoints[$user->getUsername()][$date] < $eloHistory->getNewElo()) {
+                $dataPoints[$user->getUsername()][$date]  = $eloHistory->getNewElo();
+            }
+        }
+
+        return $dataPoints;
+    }
+
 
 } 
