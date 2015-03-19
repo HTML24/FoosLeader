755a5ef3225c32f8587711f311713d88efc21181
Tomas Gudmundsson <tg@html24.net>
2015-03-19 22:31:49 +0100

    Deleting unclaimed results
    Unclaimed results removed from the system after a month.

--- src/TomGud/FoosLeader/CoreBundle/Controller/DefaultController.php
+++ src/TomGud/FoosLeader/CoreBundle/Controller/DefaultController.php
@@ -9,19 +9,33 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	$result_repo = $this->get('doctrine.orm.entity_manager')->getRepository('FoosLeaderCoreBundle:Result');
+        $em = $this->get('doctrine.orm.entity_manager');
+    	$result_repo = $em->getRepository('FoosLeaderCoreBundle:Result');
     	$latest_for_user = $result_repo->getLatestForUser($this->getUser());
     	$latest_for_all = $result_repo->getLatestForAll();
     	$unconfirmed_for_user = $result_repo->getUnconfirmedResultsForUser($this->getUser());
 
         $available_results = $result_repo->getAvailableResults();
+        $available_results_remaining = array();
+
+        $now = new \DateTime('now', new \DateTimeZone('UTC'));
+        foreach ($available_results as $result) {
+            $difference = $now->diff($result->getSubmitted());
+            if ($difference->invert === 1 && $difference->days >= 30) {
+                // Month old result not been claimed, let's delete
+                $em->remove($result);
+            } else {
+                $available_results_remaining[] = $result;
+            }
+        }
+        $em->flush();
 
         return $this->render('FoosLeaderCoreBundle:Default:index.html.twig',
         	array(
         		'latest_user' => $latest_for_user,
         		'latest_all' => $latest_for_all,
         		'unconfirmed' => $unconfirmed_for_user,
-                'availables' => $available_results,
+                'availables' => $available_results_remaining,
         		'notifications' => array()
         	)
         );
