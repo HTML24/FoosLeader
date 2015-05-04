51f7f13158e95156cf2ef3a34f1489ffdd9f1300
Tomas Gudmundsson <tg@html24.net>
2015-05-04 08:28:49 +0200

    PlayerStatisticsCollection added
    PlayerStatisticsCollection keeps track of all the PlayerStatistics. It
    can be sorted by various parameters and then looped over in a foreach.
    The statistics service is then responsible for populating the collection
    with some verbose queries, but this developer is not quite sure how to
    make it more efficient or more pretty. Make it work first though :)
    
    Upside is though, that we only have 4 queries to get all statistics for
    all players. It can be debated if there should be 4 queries per
    statistic (such as goal scored and games won) that could possibly be
    optimized to actually be fewer instead of doin sorting in PHP.

---
+++ src/TomGud/FoosLeader/CoreBundle/Model/PlayerStatisticsCollection.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomas
+ * Date: 30/04/15
+ * Time: 21:36
+ */
+
+namespace TomGud\FoosLeader\CoreBundle\Model;
+
+
+use Countable;
+use TomGud\FoosLeader\UserBundle\Entity\User;
+use Traversable;
+
+class PlayerStatisticsCollection implements  Countable, \IteratorAggregate {
+    /**
+     * @var PlayerStatisticsModel[]
+     */
+    protected $playerStatistics;
+
+    /**
+     * @var PlayerStatisticsModel[]
+     */
+    protected $sortedPlayerStatistics;
+
+    /**
+     * Sort player statistics by number of wins
+     */
+    const SORT_WIN = 0;
+
+    /**
+     * Sort player statistics by goals scored
+     */
+    const SORT_GOALS = 1;
+
+    /**
+     * Sort player statistics by win ratio
+     */
+    const SORT_WIN_RATIO = 2;
+
+    /**
+     * Sort player statistics by goal ratio
+     */
+    const SORT_GOAL_RATIO = 3;
+
+    /**
+     * Sort player statistics by average goals scored
+     */
+    const SORT_AVERAGE_SCORE = 4;
+
+    /**
+     * Sort player statistics by average goals conceded
+     */
+    const SORT_AVERAGE_CONCEDED = 5;
+
+    public function __construct()
+    {
+        $this->playerStatistics = array();
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->playerStatistics);
+    }
+
+    /**
+     * @param User $player
+     * @param $gamesWon
+     * @param $gamesLost
+     * @param $goalsScored
+     * @param $goalsConceded
+     */
+    public function addPlayerStatistics(User $player, $gamesWon, $gamesLost, $goalsScored, $goalsConceded)
+    {
+        $playerStatistics = new PlayerStatisticsModel($player);
+        $playerStatistics
+            ->addGamesWon($gamesWon)
+            ->addGamesLost($gamesLost)
+            ->addGoalsScored($goalsScored)
+            ->addGoalsConceded($goalsConceded);
+
+        if (array_key_exists($player->getId(), $this->playerStatistics)) {
+            $existingStatistics = $this->playerStatistics[$player->getId()];
+            $existingStatistics->addPlayerStatistic($playerStatistics);
+        } else {
+            $this->playerStatistics[$player->getId()] = $playerStatistics;
+        }
+    }
+
+    /**
+     * @param $sortFlag
+     * @throws \InvalidArgumentException
+     */
+    public function sortPlayers($sortFlag)
+    {
+        $this->sortedPlayerStatistics = $this->playerStatistics;
+        switch ($sortFlag)
+        {
+            case PlayerStatisticsCollection::SORT_WIN:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByWin"));
+                break;
+            case PlayerStatisticsCollection::SORT_GOALS:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByGoals"));
+                break;
+            case PlayerStatisticsCollection::SORT_WIN_RATIO:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByWinRatio"));
+                break;
+            case PlayerStatisticsCollection::SORT_GOAL_RATIO:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByGoalRatio"));
+                break;
+            case PlayerStatisticsCollection::SORT_AVERAGE_SCORE:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByAvgScored"));
+                break;
+            case PlayerStatisticsCollection::SORT_AVERAGE_CONCEDED:
+                uasort($this->sortedPlayerStatistics, array($this, "sortPlayersByAvgConceded"));
+                break;
+            default:
+                throw new \InvalidArgumentException("Only sort constants from PlayerStatisticsCollection are valid.");
+        }
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByWin($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getGamesWon() === $b->getGamesWon()) {
+                return 0;
+            }
+            return ($a->getGamesWon() > $b->getGamesWon()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByGoals($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getGoalsScored() === $b->getGoalsScored()) {
+                return 0;
+            }
+            return ($a->getGoalsScored() > $b->getGoalsScored()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByWinRatio($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getGoalRatio() === $b->getGoalRatio()) {
+                return 0;
+            }
+            return ($a->getGoalRatio() > $b->getGoalRatio()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByGoalRatio($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getWinRatio() === $b->getWinRatio()) {
+                return 0;
+            }
+            return ($a->getWinRatio() > $b->getWinRatio()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByAvgScored($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getAverageGoalsScored() === $b->getAverageGoalsScored()) {
+                return 0;
+            }
+            return ($a->getAverageGoalsScored() > $b->getAverageGoalsScored()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    /**
+     * @param PlayerStatisticsModel $a
+     * @param PlayerStatisticsModel $b
+     * @return bool
+     */
+    private function sortPlayersByAvgConceded($a, $b)
+    {
+        if ($a instanceof PlayerStatisticsModel && $b instanceof PlayerStatisticsModel) {
+            if ($a->getAverageGoalsConceded() === $b->getAverageGoalsConceded()) {
+                return 0;
+            }
+            return ($a->getAverageGoalsConceded() > $b->getAverageGoalsConceded()) ? -1 : 1;
+        }
+        return false;
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->sortedPlayerStatistics);
+    }
+}
--- src/TomGud/FoosLeader/CoreBundle/Service/StatisticsService.php
+++ src/TomGud/FoosLeader/CoreBundle/Service/StatisticsService.php
@@ -10,7 +10,11 @@ namespace TomGud\FoosLeader\CoreBundle\Service;
 
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use TomGud\FoosLeader\CoreBundle\Entity\Result;
+use TomGud\FoosLeader\CoreBundle\Model\PlayerStatisticsModel;
+use TomGud\FoosLeader\CoreBundle\Model\PlayerStatisticsCollection;
 use TomGud\FoosLeader\UserBundle\Entity\User;
 
 class StatisticsService {
@@ -19,8 +23,15 @@ class StatisticsService {
      */
     protected $em;
 
+    /**
+     * @var PlayerStatisticsCollection
+     */
+    protected $playerStatistics;
+
     public function __construct(EntityManagerInterface $em) {
         $this->em = $em;
+        $this->playerStatistics = new PlayerStatisticsCollection();
+        $this->loadGameStatistics();
     }
 
     public function getTotalGames()
@@ -59,5 +70,135 @@ class StatisticsService {
             ->orderBy('user.ELORanking', 'DESC')
             ->setMaxResults($numberOfTopPlayers);
         return new Paginator($userQueryBuilder->getQuery(), $fetchJoin = true);
+    }
+
+    /**
+     * @param int $numberOfTopPlayers
+     * @return PlayerStatisticsModel[]
+     */
+    public function getTopWonRatios($numberOfTopPlayers = 5)
+    {
+        $this->playerStatistics->sortPlayers(PlayerStatisticsCollection::SORT_WIN_RATIO);
+        $topPlayers = array();
+        foreach ($this->playerStatistics as $statistic) {
+            $topPlayers[] = $statistic;
+            if (count($topPlayers) >= $numberOfTopPlayers) {
+                break;
+            }
+        }
+        return $topPlayers;
+    }
+
+    /**
+     * Fetching all statistics necessary for players in most optimal way I know how. Doing this once only takes up
+     * four queries. Due to the way the result table is built up, we need to do in uch a backwards way. If you
+     * dear reader know of a better way, please contribute.
+     *
+     * We need the sum of all goals, and won games and loss games for every player in each position 1-4 of each result.
+     */
+    private function loadGameStatistics()
+    {
+        $monthAgo = new \DateTime('now', new \DateTimeZone('UTC'));
+        $monthAgo->sub(\DateInterval::createFromDateString('1 month'));
+        $team1WinCountsQB = $this->em->getRepository('FoosLeaderCoreBundle:Result')->createQueryBuilder('r');
+        $team1WinCountsQB
+            ->select('r', 'COUNT(r.player1) AS p1_victories',
+                'COUNT(r.player3) AS p3_victories',
+                'SUM(r.team1Score AS team1_goals, SUM(r.team2Score) AS team2_goals')
+            ->leftJoin('TomGud\FoosLeader\UserBundle\Entity\User', 'user', Join::WITH,
+                'r.player1 = user.id OR r.player2 = user.id OR r.player3 = user.id or r.player4 = user.id')
+            ->where('r.team1Score > r.team2Score')
+            ->andWhere('r.team1Confirmed = 1 AND r.team2Confirmed = 1')
+            ->andWhere('user.lastLogin > :monthAgo')
+            ->setParameter('monthAgo', $monthAgo)
+            ->groupBy('r.player1', 'r.player3');
+
+        $team1LossCountsQB = $this->em->getRepository('FoosLeaderCoreBundle:Result')->createQueryBuilder('r');
+        $team1LossCountsQB
+            ->select('r', 'COUNT(r.player1) AS p1_losses',
+                'COUNT(r.player3) AS p3_losses',
+                'SUM(r.team1Score AS team1_goals, SUM(r.team2Score) AS team2_goals')
+            ->leftJoin('TomGud\FoosLeader\UserBundle\Entity\User', 'user', Join::WITH,
+                'r.player1 = user.id OR r.player2 = user.id OR r.player3 = user.id or r.player4 = user.id')
+            ->where('r.team1Score < r.team2Score')
+            ->andWhere('r.team1Confirmed = 1 AND r.team2Confirmed = 1')
+            ->andWhere('user.lastLogin > :monthAgo')
+            ->setParameter('monthAgo', $monthAgo)
+            ->groupBy('r.player1', 'r.player3');
+
+        $team2WinCountsQB = $this->em->getRepository('FoosLeaderCoreBundle:Result')->createQueryBuilder('r');
+        $team2WinCountsQB
+            ->select('r', 'COUNT(r.player2) AS p2_victories',
+                'COUNT(r.player4) AS p4_victories',
+                'SUM(r.team1Score AS team1_goals, SUM(r.team2Score) AS team2_goals')
+            ->leftJoin('TomGud\FoosLeader\UserBundle\Entity\User', 'user', Join::WITH,
+                'r.player1 = user.id OR r.player2 = user.id OR r.player3 = user.id or r.player4 = user.id')
+            ->where('r.team1Score < r.team2Score')
+            ->andWhere('r.team1Confirmed = 1 AND r.team2Confirmed = 1')
+            ->andWhere('user.lastLogin > :monthAgo')
+            ->setParameter('monthAgo', $monthAgo)
+            ->groupBy('r.player2', 'r.player4');
+
+        $team2LossCountsQB = $this->em->getRepository('FoosLeaderCoreBundle:Result')->createQueryBuilder('r');
+        $team2LossCountsQB
+            ->select('r', 'COUNT(r.player2) AS p2_losses',
+                'COUNT(r.player4) AS p4_losses',
+                'SUM(r.team1Score AS team1_goals, SUM(r.team2Score) AS team2_goals')
+            ->leftJoin('TomGud\FoosLeader\UserBundle\Entity\User', 'user', Join::WITH,
+                'r.player1 = user.id OR r.player2 = user.id OR r.player3 = user.id or r.player4 = user.id')
+            ->where('r.team1Score > r.team2Score')
+            ->andWhere('r.team1Confirmed = 1 AND r.team2Confirmed = 1')
+            ->andWhere('user.lastLogin > :monthAgo')
+            ->setParameter('monthAgo', $monthAgo)
+            ->groupBy('r.player2', 'r.player4');
+
+        $team1WinCountsResults = $team1WinCountsQB->getQuery()->getResult();
+        $team1LossCountsResults = $team1LossCountsQB->getQuery()->getResult();
+        $team2WinCountsResults = $team2WinCountsQB->getQuery()->getResult();
+        $team2LossCountsResults = $team2LossCountsQB->getQuery()->getResult();
+
+        foreach ($team1WinCountsResults as $statisticsResult) {
+            /* @var Result $result */
+            $result = $statisticsResult[0];
+            $this->playerStatistics->addPlayerStatistics($result->getPlayer1(), $statisticsResult['p1_victories'], 0,
+                $statisticsResult['team1_goals'], $statisticsResult['team2_goals']);
+            if ($result->getPlayer3() !== null && $result->getPlayer4() !== null) {
+                $this->playerStatistics->addPlayerStatistics($result->getPlayer3(), $statisticsResult['p3_victories'],
+                    0,$statisticsResult['team1_goals'], $statisticsResult['team2_goals']);
+            }
+        }
+
+        foreach ($team2WinCountsResults as $statisticsResult) {
+            /* @var Result $result */
+            $result = $statisticsResult[0];
+            $this->playerStatistics->addPlayerStatistics($result->getPlayer2(), $statisticsResult['p2_victories'], 0,
+                $statisticsResult['team2_goals'], $statisticsResult['team1_goals']);
+            if ($result->getPlayer4() !== null) {
+                $this->playerStatistics->addPlayerStatistics($result->getPlayer4(), $statisticsResult['p2_victories'], 0,
+                    $statisticsResult['team2_goals'], $statisticsResult['team1_goals']);
+            }
+        }
+
+        foreach ($team1LossCountsResults as $statisticsResult) {
+            /* @var Result $result */
+            $result = $statisticsResult[0];
+            $this->playerStatistics->addPlayerStatistics($result->getPlayer1(), 0, $statisticsResult['p1_losses'],
+                $statisticsResult['team1_goals'], $statisticsResult['team2_goals']);
+            if ($result->getPlayer3() !== null) {
+                $this->playerStatistics->addPlayerStatistics($result->getPlayer3(), 0, $statisticsResult['p1_losses'],
+                    $statisticsResult['team1_goals'], $statisticsResult['team2_goals']);
+            }
+        }
+
+        foreach ($team2LossCountsResults as $statisticsResult) {
+            /* @var Result $result */
+            $result = $statisticsResult[0];
+            $this->playerStatistics->addPlayerStatistics($result->getPlayer2(), 0, $statisticsResult['p2_losses'],
+                $statisticsResult['team2_goals'], $statisticsResult['team1_goals']);
+            if ($result->getPlayer4() !== null) {
+                $this->playerStatistics->addPlayerStatistics($result->getPlayer4(), 0, $statisticsResult['p2_losses'],
+                    $statisticsResult['team2_goals'], $statisticsResult['team1_goals']);
+            }
+        }
     }
 }
