d08456761c3e495db92dbf96fe0340246ec4cd9f
Tomas Gudmundsson <togu@dr.dk>
2016-03-30 22:39:01 +0200

    Fixing FOSUserBundle alias service bug
    See https://github.com/FriendsOfSymfony/FOSUserBundle/issues/1775
    for more information.

--- src/TomGud/FoosLeader/UserBundle/Entity/User.php
+++ src/TomGud/FoosLeader/UserBundle/Entity/User.php
@@ -4,7 +4,7 @@ namespace TomGud\FoosLeader\UserBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use TomGud\FoosLeader\CoreBundle\Entity\Result;
 
